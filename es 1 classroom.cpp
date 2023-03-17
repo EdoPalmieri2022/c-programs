06c8c5613038e164f41c3c709dc3c32bc4ee6394
doPalmieri2022 <edopalmieri21@gmail.com>
2023-03-17 19:59:39 +0100

    aaa

--- es 1 classroom.cpp
+++ es 1 classroom.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
+int main()
+{
 	int a;
-	cin>>a;
-cout<<"hello world";
-cout<<"pippo e pluto";
+	cin >> a;
+	cout << "hello world";
+	cout << "pippo e pluto";
 }
